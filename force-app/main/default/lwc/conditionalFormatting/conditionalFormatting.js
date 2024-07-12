1914c7cb374983ab543f0d132e1da26440576a83
Vishvanath Hiremath <vishvamh@gmail.com>
2024-07-12 15:14:34 -0500

    getters in directives

--- force-app/main/default/lwc/conditionalFormatting/conditionalFormatting.js
+++ force-app/main/default/lwc/conditionalFormatting/conditionalFormatting.js
@@ -2,8 +2,17 @@ import { LightningElement } from 'lwc';
 
 export default class ConditionalFormatting extends LightningElement {
     isVisible = false
+    name
     
     showInfo() {
         this.isVisible = true
     }
+
+    handleChange(event) {
+        this.name = event.target.value
+    }
+    get showdetails() {
+        return this.name === 'hello'
+    }
+
 }
