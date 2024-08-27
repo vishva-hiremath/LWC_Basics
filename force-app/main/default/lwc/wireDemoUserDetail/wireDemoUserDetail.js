0fe91f10f3be83453dc5d8aad84db945d9644015
vishva-hiremath <vishvamh@gmail.com>
2024-08-27 15:38:24 -0500

    Wire Adapters and Functions

---
+++ force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -0,0 +1,27 @@
+import { LightningElement, wire } from 'lwc';
+import id from '@salesforce/user/Id';
+import { getRecord } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+import EMAIL_FIELD from '@salesforce/schema/User.Email';
+const fields =[NAME_FIELD, EMAIL_FIELD]
+
+
+export default class WireDemoUserDetail extends LightningElement {
+
+    userId = id
+    //005ak000003V7ZlAAK
+    
+    userDetails
+    @wire(getRecord, { recordId:'005ak000003V7ZlAAK', fields:[NAME_FIELD, 'User.Email', 'User.Phone']})
+    userDetailHandler({data, error}) {
+        if (data) {
+            this.userDetails = data.fields;
+        }
+        if (error) {
+            console.error(error)
+        }
+    };
+
+    @wire(getRecord, { recordId: '005ak000003V7ZlAAK', fields: fields })
+    userProperty
+}
